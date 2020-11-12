4affd032c5e0e4469c15fa16445a834cfd25761d
Benjamin <benjamin.voigt@btinet.org>
2020-11-12 16:13:14 +0100

    update entire project

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,126 +1,131 @@
-<?php 
+<?php
 
-/*
- * (c) 2019 BENJAMIN VOIGT <benjamin.voigt@btinet.org>
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
+namespace Btinet\Baldur;
 
-namespace  Btinet\ SimpleMVC;
 
-use Btinet\ SimpleMVC\ Core;
-use Btinet\ SimpleMVC\ Modules;
-
-use Twig\ Loader;
+use Exception;
+use Symfony\Component\Dotenv\Dotenv;
 
 class Bootstrap {
 
-	public $_post;
-	private $_url;
-	private $_filter;
-	private $_controller = null;
-	private $_defaultController;
+    private array $url;
+    private $controller = null;
+    private $defaultController;
 
-	public
+    /**
+     * Kernel constructor.
+     */
+    function __construct()
+    {
+        $dotenv = new Dotenv();
+        $dotenv->loadEnv(dirname(__DIR__) . '/.env');
 
-	function __construct() {
-		
-		Modules\ Session::init();
-		
-		$this->_getUrl();
-		$this->_getPOST();
+        if (isset($_ENV['APP_ENV'])) {
+            switch ($_ENV['APP_ENV']) {
+                case 'development':
+                    error_reporting(E_ALL);
+                    break;
+                case 'production':
+                    error_reporting(0);
+                    break;
+                default:
+                    die('The application environment "'.$_ENV['APP_ENV'].'" is not allowed. Allowed: "development" or "production" ');
+            }
+        }
 
-	}
+        $this->parseUrl();
+    }
 
-	public
+    /**
+     * @param $name
+     */
+    public function setController($name) {
+        $this->defaultController = $name;
+    }
 
-	function setController( $name ) {
-		
-		$this->_defaultController = $name;
-	}
+    public function init() {
+        //if no page requested set default controller
+        if(empty($this->url[0])) {
+            $this->loadDefaultController();
+            return false;
+        }
+        $this->loadExistingController();
+        $this->callControllerMethod();
+        return false;
+    }
 
-	public
+    private function parseUrl() {
+        $url = isset($_GET['url']) ? rtrim($_GET['url'], '/') : NULL;
+        $url = urldecode(filter_var(urlencode($url), FILTER_SANITIZE_URL));
+        $this->url = explode('/', $url);
+    }
 
-	function init() {
-					
-		if ( empty( $this->_url[ 0 ] ) ) {
-			$this->_loadDefaultController();
-			return false;
-		}
+    private function loadDefaultController() {
+        $this->controller = new $this->defaultController();
+        $this->controller->index();
+    }
 
-		$this->_loadExistingController();
-		$this->_callControllerMethod();
-	}
+    private function loadExistingController() {
 
-	private
 
-	function _getUrl() {
-		$url = isset( $_GET[ 'url' ] ) ? rtrim( $_GET[ 'url' ], '/' ) : NULL;
-		$url = urldecode( filter_var( urlencode( $url ), FILTER_SANITIZE_URL ) );
-		$this->_url = explode( '/', $url );
-	}
+        $this->controller = 'App\\Controller\\'.ucfirst($this->url[0]).'Controller';
 
-	public
+        try {
+            if (class_exists($this->controller)){
+                $this->controller = new $this->controller();
+            } else {
+                throw new Exception('Controller-Klasse gibts nicht, daher m&ouml;chte ich nun eine Fehlerseite ausgeben.');
+            }
+        } catch(Exception $e) {
+            Logger::newMessage($e);
+            Logger::customErrorMsg($e);
+        }
 
-	function _getPOST() {
-		$post = isset( $_POST ) ? $_POST : NULL;
-		$this->_post = filter_var_array( $post, FILTER_SANITIZE_STRING );
-	}
 
-	private
+    }
 
-	function _loadDefaultController() {
-		$this->_defaultController = APPSPACE . $this->_defaultController;
-		$this->_controller = new $this->_defaultController();
-		$this->_controller->index();
-	}
+    /**
+     * If a method is passed in the GET url paremter
+     *
+     *  http://localhost/controller/method/(param)/(param)/(param)
+     *  url[0] = Controller
+     *  url[1] = Method
+     *  url[2] = Parameter
+     *  url[3] = Parameter
+     *  url[4] = Parameter
+     */
+    private function callControllerMethod()
+    {
+        unset($this->url[0]);
+        $method = (empty($this->url[1])) ? 'index' : $this->url[1];
 
-	private
+        try {
+            if (is_callable(array($this->controller, $method))) {
+                $method = (is_array($method)) ? array_shift($this->url) : $method;
+            } else {
+                throw new Exception('Methode gibts nicht, daher m&ouml;chte ich nun eine Fehlerseite ausgeben.');
+            }
+        } catch(Exception $e) {
+            // TODO: Add Error Page
+            Logger::newMessage($e);
+            Logger::customErrorMsg($e);
+        }
 
-	function _loadExistingController() {
 
-		//set url for controllers
-		$controller = APPSPACE . ucfirst($this->_url[ 0 ]);
+        $parameter = filter_var_array($this->url, FILTER_SANITIZE_STRING);
+        call_user_func_array(array($this->controller, $method), $parameter);
+    }
 
-		if ( class_exists( $controller ) ) {
-			
-			//instatiate controller
-			$this->_controller = new $controller;
-		} else {
-			$this->_errors( "File does not exist: " . $this->_url[ 0 ] );
-			return false;
-		}
-
-	}
-
-	private
-
-	function _callControllerMethod() {
-		unset( $this->_url[ 0 ] );
-		$method = 'index';
-
-		if (isset($this->_url[ 1 ])){
-			
-			if ( is_callable( array( $this->_controller, $this->_url[ 1 ] ) ) ) {
-				$method = array_shift( $this->_url );
-			}
-			
-		} 
-
-		$parameter[ 'get' ] = filter_var_array( $this->_url, FILTER_SANITIZE_STRING );
-		$parameter[ 'post' ] = $this->_post;
-
-		call_user_func_array( array( $this->_controller, $method ), $parameter );
-	}
-
-	private
-
-	function _errors( $error ) {
-
-		$this->_controller = new Core\ Errors( $error );
-		$this->_controller->index();
-		die;
-	}
+    /**
+     * Display an error page if nothing exists
+     *
+     * @param $error
+     * @return void
+     */
+    private function error($error) {
+        Logger::exception_handler($error);
+        Logger::errorMessage($error, true);
+        die;
+    }
 
 }
